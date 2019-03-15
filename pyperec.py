b91871cde07c25eec6b9c1314b44a0d4eab82fc4
cspaier <clement.spaier@gmail.com>
2019-03-15 13:39:07 +0100

    cleanup pypererec.py

--- pyperec.py
+++ pyperec.py
@@ -2,7 +2,8 @@ from igraph import *
 import unidecode
 import plotly.plotly as py
 from plotly.graph_objs import *
-from plotly import tools
+
+
 NUMBER_LETTER = 5
 
 
@@ -34,89 +35,99 @@ def build_graph(words_list):
     return g
 
 def show_graph(g):
+    """Simple graph rendering using igraph plotself.
+
+    https://igraph.org/python/doc/tutorial/tutorial.html#drawing-a-graph-using-a-layout
+    """
     layout = g.layout("lgl")
     plot(g, layout = layout)
 
-#words_list = read_dictionary_file("liste_francais1.txt")
-#g = build_graph(words_list)
-#g.write_pickle("graph.pickle")
+def export_to_iplot(G):
+    """ Export graph to plotly for interactive sharing.
 
-G = load("graph.pickle")
-labels=list(G.vs['name'])
-N=len(labels)
-E=[e.tuple for e in G.es]# list of edges
-layt = G.layout()#kamada-kawai layou
-print(type(layt))
-Xn=[layt[k][0] for k in range(N)]
-Yn=[layt[k][1] for k in range(N)]
-Xe=[]
-Ye=[]
-for e in E:
-    Xe+=[layt[e[0]][0],layt[e[1]][0], None]
-    Ye+=[layt[e[0]][1],layt[e[1]][1], None]
+    You need an account on plotly: https://plot.ly/python/getting-started/
+    Using sample code from this exemple: https://plot.ly/python/igraph-networkx-comparison/
+    """
+    labels=list(G.vs['name'])
+    N=len(labels)
+    E=[e.tuple for e in G.es]# list of edges
+    layt = G.layout()#kamada-kawai layout is very slow to render on my machine.
+    Xn=[layt[k][0] for k in range(N)]
+    Yn=[layt[k][1] for k in range(N)]
+    Xe=[]
+    Ye=[]
+    for e in E:
+        Xe+=[layt[e[0]][0],layt[e[1]][0], None]
+        Ye+=[layt[e[0]][1],layt[e[1]][1], None]
 
-trace1=Scatter(x=Xe,
-               y=Ye,
-               mode='lines',
-               line= dict(color='rgb(210,210,210)', width=1),
-               hoverinfo='none'
-               )
-trace2=Scatter(x=Xn,
-               y=Yn,
-               mode='markers',
-               name='ntw',
-               marker=dict(symbol='circle-dot',
-                                        size=5,
-                                        color='#6959CD',
-                                        line=dict(color='rgb(50,50,50)', width=0.5)
-                                        ),
-               text=labels,
-               hoverinfo='text'
-               )
+    trace1=Scatter(x=Xe,
+                   y=Ye,
+                   mode='lines',
+                   line= dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+                   )
+    trace2=Scatter(x=Xn,
+                   y=Yn,
+                   mode='markers',
+                   name='ntw',
+                   marker=dict(symbol='circle-dot',
+                                            size=5,
+                                            color='#6959CD',
+                                            line=dict(color='rgb(50,50,50)', width=0.5)
+                                            ),
+                   text=labels,
+                   hoverinfo='text'
+                   )
 
-axis=dict(showline=False, # hide axis line, grid, ticklabels and  title
-          zeroline=False,
-          showgrid=False,
-          showticklabels=False,
-          title=''
-          )
+    axis=dict(showline=False, # hide axis line, grid, ticklabels and  title
+              zeroline=False,
+              showgrid=False,
+              showticklabels=False,
+              title=''
+              )
 
-width=800
-height=800
-layout=Layout(title= "pyperec test 2",
-    font= dict(size=12),
-    showlegend=False,
-    autosize=False,
-    width=width,
-    height=height,
-    xaxis=layout.XAxis(axis),
-    yaxis=layout.YAxis(axis),
-    margin=layout.Margin(
-        l=40,
-        r=40,
-        b=85,
-        t=100,
-    ),
-    hovermode='closest',
-    annotations=[
-           dict(
-           showarrow=False,
-            text='From a 2401 list of 5 letters french words. Proper nouns not removed.',
-            xref='paper',
-            yref='paper',
-            x=0,
-            y=-0.1,
-            xanchor='left',
-            yanchor='bottom',
-            font=dict(
-            size=14
-            )
-            )
-        ]
-    )
+    width=800
+    height=800
+    layout=Layout(title= "pyperec test 2",
+        font= dict(size=12),
+        showlegend=False,
+        autosize=False,
+        width=width,
+        height=height,
+        xaxis=layout.XAxis(axis),
+        yaxis=layout.YAxis(axis),
+        margin=layout.Margin(
+            l=40,
+            r=40,
+            b=85,
+            t=100,
+        ),
+        hovermode='closest',
+        annotations=[
+               dict(
+               showarrow=False,
+                text='From a 2401 list of 5 letters french words. Proper nouns not removed.',
+                xref='paper',
+                yref='paper',
+                x=0,
+                y=-0.1,
+                xanchor='left',
+                yanchor='bottom',
+                font=dict(
+                size=14
+                )
+                )
+            ]
+        )
 
-data=[trace1, trace2]
-fig=Figure(data=data, layout=layout)
-print(fig)
+    data=[trace1, trace2]
+    fig=Figure(data=data, layout=layout)
 
-py.iplot(fig, filename='Pyperec-test-2')
+    py.iplot(fig, filename='Pyperec-test-2')
+
+
+# Exemples
+# words_list = read_dictionary_file("french/liste_french.txt")
+# g = build_graph(words_list)
+# g.write_pickle("graph.pickle")
+# g = load("graph.pickle")
